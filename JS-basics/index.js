183a7e3ee76991390228698b524f66c63fc7f8d3
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-08-22 13:09:12 +0200

    javascriptBasics

---
+++ JS-basics/index.js
@@ -0,0 +1,18 @@
+let firstName = 'Alexander';
+let lastName = 'Polzhofer';
+
+console.log(firstName + ' ' + lastName);
+
+/*const (= constant can't change it's value)*/
+
+const intersestRateEurope = 3;
+
+console.log(intersestRateEurope);
+
+/*let (= to declare a variable it can change it's value)*/
+
+let interestRateUSA = 5;
+interestRate = 3;
+
+console.log(interestRate);
+
