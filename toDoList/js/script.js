7b404723e9c9f9e83aee043cc27381217e938a27
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-08-26 19:45:02 +0200

    adding checkbox

--- toDoList/js/script.js
+++ toDoList/js/script.js
@@ -1,12 +1,30 @@
-const tasks = [''];
+var tasks = [];
 
 printTaskList();
 
 document.getElementById("addTask").addEventListener("click", function () {
-    let newTask = document.getElementById("txtNewTask").value;
-    tasks.push(newTask);
-    printTaskList();
+    addTask();
 });
+
+function addTask() {
+    let newTask = document.getElementById("txtNewTask").value;
+    let taskResponsible = document.getElementById("txtResponsible").value;
+    let task = {
+        name: newTask,
+        responsible: taskResponsible,
+        isDone: false
+    };
+    tasks.push(task);
+    printTaskList();
+}
+
+function markTask(element) {
+    let index = element.attributes['data-index'].value;
+    let isChecked = element.checked;
+
+    tasks[index].isDone = isChecked;
+    printTaskList();
+}
 
 function printTaskList() {
     document.getElementById("taskList").innerHTML = getHTMLTasks();
@@ -14,9 +32,15 @@ function printTaskList() {
 
 function getHTMLTasks() {
     let html = "";
+    let index = "";
 
     tasks.forEach(element => {
-        html += "<li>" + element + "</li>";
+        let checked = "";
+        if (element.isDone) {
+            checked = "checked";
+        }
+        html += "<li><input onClick='markTask(this)' name = 'checkbox' data-index = '" + index + "' type ='checkbox' '" + checked + "'/> " + element.name + ' | ' + element.responsible + "</li > ";
+        index++;
     });
     return html;
 }
