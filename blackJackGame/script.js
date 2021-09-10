d4c8c0e2e92a026f50bbde026a4c84e2198a81d0
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-09-10 14:59:38 +0200

    update blackJackGame

--- blackJackGame/script.js
+++ blackJackGame/script.js
@@ -1,12 +1,29 @@
 let firstCard = 5;
-let secondCard = 6;
-
+let secondCard = 8;
 let sum = firstCard + secondCard;
+let message = "";
 
-if (sum < 21) {
-    console.log("Do you want a new card?")
-} else if (sum === 21) {
-    console.log("Whooohohoho .. you've got blackJack!")
-} else {
-    console.log("You're out of the game!")
+let hasBlackJack = false;
+let isAlive = true;
+let messageEl = document.getElementById("message-el");
+
+function clickOnButton() {
+    document.getElementById("button").addEventListener("click", function () {
+        startGame();
+    })
 }
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want a new card?"
+    } else if (sum === 21) {
+        message = "Whooohohoho .. you've got blackJack!"
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!"
+        isAlive = false;
+    }
+    messageEl.textContent=message;
+}
+
+clickOnButton();
