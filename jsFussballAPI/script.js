ffd0a682b2313df0a7c77e170e38e37d3db348c0
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-09-18 18:02:50 +0200

    adding buttons

--- jsFussballAPI/script.js
+++ jsFussballAPI/script.js
@@ -1,11 +1,28 @@
-
-
 let urlTeamsBundesliga = 'https://api.football-data.org/v2/competitions/2002/teams';
 let urlTeamsPremierLeague = 'https://api.football-data.org/v2/teams';
 let urlStandings = 'https://api.football-data.org/v2/competitions/2002/standings?season=2019';
 
 var source = document.getElementById("team-template").innerHTML;
 var template = Handlebars.compile(source);
+
+var standingSource = document.getElementById("standings-template").innerHTML;
+var templateStandings = Handlebars.compile(standingSource);
+
+function loadStandings() {
+	fetch(urlStandings, {
+		headers: {
+			"x-auth-token": "819babcd7902454f930c154272296d78"
+		}
+	}).then(response => response.json())
+		.then(function (data) {
+			let html = "";
+			data.matches.forEach(element => {
+				html += templateStandings(element);
+			});
+			document.getElementById("standings").innerHTML = html;
+		});
+}
+
 
 function loadPremierLeagueTeams() {
 
@@ -17,8 +34,7 @@ function loadPremierLeagueTeams() {
 		.then(function (data) {
 			let html = "";
 			data.teams.forEach(element => {
-				console.log(template(element))
-				html += "<li><img src='" + element.crestUrl + "'/>" + element.id + " " + element.name + " " + " " + element.address + " " + element.founded + "</li>";
+				html += template(element);
 			});
 			document.getElementById("teamsPremierLeague").innerHTML = html;
 		}).catch(function (err) {
@@ -48,6 +64,24 @@ function loadBundesligaTeams() {
 		});
 }
 
-//loadPremierLeagueTeams();
+function onClickBundesligaButton() {
+	document.getElementById("buttonBundesliga").addEventListener("click", function () {
+		loadBundesligaTeams();
+	})
+}
 
-loadBundesligaTeams();
+function onClickPremierLeagueButton() {
+	document.getElementById("buttonPremierLeague").addEventListener("click", function () {
+		loadPremierLeagueTeams();
+	})
+}
+
+function onClickStandingsButton() {
+	document.getElementById("buttonStandings").addEventListener("click", function () {
+		loadStandings();
+	})
+}
+
+onClickPremierLeagueButton();
+onClickBundesligaButton();
+onClickStandingsButton();
