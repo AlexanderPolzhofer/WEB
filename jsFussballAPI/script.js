01df6837e4bdd6dd84a91c5d8705aa6b58f0f04f
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-09-23 21:59:18 +0200

    adding bootstrap

--- jsFussballAPI/script.js
+++ jsFussballAPI/script.js
@@ -1,6 +1,9 @@
 let urlTeamsBundesliga = 'https://api.football-data.org/v2/competitions/2002/teams';
 let urlTeamsPremierLeague = 'https://api.football-data.org/v2/teams';
 let urlStandings = 'https://api.football-data.org/v2/competitions/2002/standings?season=2019';
+let urlMatches = 'https://api.football-data.org/v2/matches';
+
+let urlCountriesRapidAPI = 'https://api-football-beta.p.rapidapi.com/countries';
 
 var source = document.getElementById("team-template").innerHTML;
 var template = Handlebars.compile(source);
@@ -8,24 +11,56 @@ var template = Handlebars.compile(source);
 var standingSource = document.getElementById("standings-template").innerHTML;
 var templateStandings = Handlebars.compile(standingSource);
 
+var countriesTemplate = document.getElementById("countries-template").innerHTML;
+var templateCountries = Handlebars.compile(countriesTemplate);
+
+function loading(){
+	return '<svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg"><circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle></svg>';
+}
+
+function loadCountries() {
+	document.getElementById("content").innerHTML = loading();
+	fetch(urlCountriesRapidAPI, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "api-football-beta.p.rapidapi.com",
+			"x-rapidapi-key": "d347e19500msh01fe56559b72defp1a7d78jsn897bfe958df2"
+		}
+	}).then(response => response.json())
+		.then(function (data) {
+			let html = "<table class = 'table'>";
+			data.response.forEach(element => {
+				html += templateCountries(element);
+			});
+
+			html += "</table>";
+			document.getElementById("content").innerHTML = html;
+		})
+		.catch(err => {
+			console.warn("Something went wrong:" + err);
+		});
+}
+
 function loadStandings() {
+	document.getElementById("content").innerHTML = "loading...";
 	fetch(urlStandings, {
 		headers: {
 			"x-auth-token": "819babcd7902454f930c154272296d78"
 		}
 	}).then(response => response.json())
 		.then(function (data) {
-			let html = "";
-			data.matches.forEach(element => {
+			let html = "<table class ='table'><thead class='thead-dark'><tr><th scope='col'></th><th scope='col'>Team</th><th scope='col'>Position</th><th scope='col'>Punkte</th></tr></thead><tbody>";
+			data.standings[0].table.forEach(element => {
 				html += templateStandings(element);
 			});
-			document.getElementById("standings").innerHTML = html;
+
+			html += "</tbody></table>";
+			document.getElementById("content").innerHTML = html;
 		});
 }
 
-
 function loadPremierLeagueTeams() {
-
+	document.getElementById("content").innerHTML = loading();
 	fetch(urlTeamsPremierLeague, {
 		headers: {
 			"x-auth-token": "819babcd7902454f930c154272296d78"
@@ -36,14 +71,14 @@ function loadPremierLeagueTeams() {
 			data.teams.forEach(element => {
 				html += template(element);
 			});
-			document.getElementById("teamsPremierLeague").innerHTML = html;
+			document.getElementById("content").innerHTML = html;
 		}).catch(function (err) {
 			console.warn("Something went wrong: " + err)
 		});
 }
 
 function loadBundesligaTeams() {
-
+	document.getElementById("content").innerHTML = "loading...";
 	var source = document.getElementById("team-template").innerHTML;
 	var template = Handlebars.compile(source);
 
@@ -58,7 +93,7 @@ function loadBundesligaTeams() {
 			data.teams.forEach(element => {
 				html += template(element);
 			});
-			document.getElementById("teamsBundesliga").innerHTML = html;
+			document.getElementById("content").innerHTML = html;
 		}).catch(function (err) {
 			console.warn("Something went wrong: " + err)
 		});
@@ -82,6 +117,13 @@ function onClickStandingsButton() {
 	})
 }
 
+function onClickCountriesButton() {
+	document.getElementById("buttonCountries").addEventListener("click", function () {
+		loadCountries();
+	})
+}
+
 onClickPremierLeagueButton();
 onClickBundesligaButton();
 onClickStandingsButton();
+onClickCountriesButton();
