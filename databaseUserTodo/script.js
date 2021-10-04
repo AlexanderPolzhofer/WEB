0de10005032f4aa510a19fab0b742ef1779cef1a
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-10-04 22:01:16 +0200

    todo method for database connection

--- databaseUserTodo/script.js
+++ databaseUserTodo/script.js
@@ -1,7 +1,15 @@
-function clickOnButton(){
-    document.getElementById("button-test").addEventListener("click", function(){
-        alert("test");
-    })
+var urlTodos = "http://localhost:8080/get/todos";
+
+function loadTodos() {
+    fetch(urlTodos)
+        .then(response => response.json())
+        .then(function (data) {
+            let html = "";
+            data.forEach(element => {
+                html += "<div class='row gx-5'><div class='col'><div class='p-3 border bg-light'>" + element.name + "</div></div></div>";
+            });
+            document.getElementById("todolist").innerHTML = html;
+        });
 }
 
-clickOnButton();
+loadTodos();
