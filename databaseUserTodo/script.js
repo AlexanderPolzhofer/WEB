ede19b0dffa475fb9d21dbd9c43333ea376369f3
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-10-09 11:10:51 +0200

    todo demo view

--- databaseUserTodo/script.js
+++ databaseUserTodo/script.js
@@ -1,4 +1,5 @@
 var urlTodos = "http://localhost:8080/get/todos";
+var urlInsertIntoTodos = "http://localhost:8080/post/todos";
 
 function loadTodos() {
     fetch(urlTodos)
@@ -9,6 +10,27 @@ function loadTodos() {
                 html += "<div class='row gx-5'><div class='col'><div class='p-3 border bg-light'>" + element.name + "</div></div></div>";
             });
             document.getElementById("todolist").innerHTML = html;
+        });
+}
+
+document.getElementById("save-button").addEventListener("click", function () {
+    saveTodo();
+})
+
+
+function saveTodo() {
+
+    let todo = { "name": document.getElementById("text-input").value };
+
+    fetch(urlInsertIntoTodos, {
+        method: "POST",
+        body: JSON.stringify(todo),
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+        .then(function (data) {
+            loadTodos();
         });
 }
 
